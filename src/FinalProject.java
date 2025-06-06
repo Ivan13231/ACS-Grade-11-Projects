95d9f8e97f957047c1c230a6a36d391d28617285
Ivan <i.tochev26@acsbg.org>
2025-06-06 08:37:45 +0300

    F1

---
+++ src/FinalProject.java
@@ -0,0 +1,21 @@
+public class FinalProject  {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class F1{
+
+}
+
+class Manufacturer extends F1{
+
+}
+
+class Tyre{
+
+}
+
+class RaceTrack{
+
+}
