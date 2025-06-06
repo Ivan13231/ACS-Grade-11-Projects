5cad19303c94ed210567e67de4c8425b6173d316
Ivan <i.tochev26@acsbg.org>
2025-06-06 08:56:00 +0300

    Tyres

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -13,6 +13,36 @@ class Manufacturer extends F1{
 }
 
 class Tyre{
+    char type;
+    int life;
+    int grip;
+
+    public Tyre(char type, int life, int grip){
+        this.type = type;
+        this.life = 15;
+        this.grip = 15;
+
+    }
+
+    public char getType(){
+        return type;
+    }
+
+    public int getLife(){
+        if(getType() == 'H')
+            return life;
+
+        else
+        {if(getType() == 'M')
+                    return life-5;
+         else if (getType() == 'S')
+
+            return life-10;
+
+            }
+
+    return 0;
+    }
 
 }
 
